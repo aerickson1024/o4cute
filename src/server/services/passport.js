1e527038d276c3de80f07da6a80092cd61e3a6d9
Andy Erickson <andy.w.erickson@gmail.com>
2017-02-18 17:02:33 -0600

    finished the passport config file

--- src/server/services/passport.js
+++ src/server/services/passport.js
@@ -5,8 +5,10 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Setup options for JWT Strategy
+// Tell Strategy where to look for the jwt token
 const jwtOptions = {
-  
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
 };
 
 // Create JWT Strategy
@@ -32,3 +34,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 // Tell passport to use this strategy
+passport.use(jwtLogin);
