d284a3def000062dc5cbb6bd2d552fba178572a2
Andy Erickson <andy.w.erickson@gmail.com>
2017-02-17 20:19:54 -0600

    stubbed out Authentication file

--- router.js
+++ router.js
@@ -1,4 +1,9 @@
+const Authentication = require('./src/server/controllers/authentication');
+
 module.exports = function(app) {
+  app.post('/signup', Authentication.signup);
+  
+  // catches everything else
   app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
   });
