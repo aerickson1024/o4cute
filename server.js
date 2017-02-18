efdb1dbb38ce025ea4addd67eb5479619c6d6235
Andy Erickson <andy.w.erickson@gmail.com>
2017-02-17 19:44:23 -0600

    added user Schema and some basic routing

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
-app.set('port', process.env.PORT || 3000);
-
+// App Setup
 app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json({ type: '*/*' }));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+router(app);
 
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-});
-
+// Server Setup
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
   console.log(`Server is now listening on port ${app.get('port')}`);
 });
